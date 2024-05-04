f9f640064afd83a44004117b79eefa1ba0079ee4
nixs-dev <joabewick@gmail.com>
2024-05-04 18:51:53 -0300

    Controller de Dentista renomeado para forma singular

--- src/Controllers/DentistaController.cs
+++ src/Controllers/DentistaController.cs
@@ -1,22 +1,144 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using OdontoSchedule.Models;
-
 
 namespace OdontoSchedule.Controllers
 {
     public class DentistaController : Controller
     {
-        private readonly DBContext context;
+        private readonly DBContext _context;
 
         public DentistaController(DBContext context)
         {
-            this.context = context;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return Ok(this.context.Dentistas.ToList<Dentista>());
+            return View(await _context.Dentistas.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dentista = await _context.Dentistas
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (dentista == null)
+            {
+                return NotFound();
+            }
+
+            return View(dentista);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Nome,CPF,DataNascimento,CRO,Especialidade")] Dentista dentista)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dentista);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dentista);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dentista = await _context.Dentistas.FindAsync(id);
+            if (dentista == null)
+            {
+                return NotFound();
+            }
+            return View(dentista);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Nome,CPF,DataNascimento,CRO,Especialidade")] Dentista dentista)
+        {
+            if (id != dentista.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dentista);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DentistaExists(dentista.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dentista);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dentista = await _context.Dentistas
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (dentista == null)
+            {
+                return NotFound();
+            }
+
+            return View(dentista);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dentista = await _context.Dentistas.FindAsync(id);
+            if (dentista != null)
+            {
+                _context.Dentistas.Remove(dentista);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DentistaExists(int id)
+        {
+            return _context.Dentistas.Any(e => e.ID == id);
         }
     }
 }
