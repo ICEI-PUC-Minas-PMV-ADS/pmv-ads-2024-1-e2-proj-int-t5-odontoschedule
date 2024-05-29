0105fc86f9de7d332b1e7ccaeedc84b70dfe6cca
nixs-dev <joabewick@gmail.com>
2024-05-29 19:20:22 -0300

    Implementação de responsividade para dispositivos moveis na áreas principais da aplicação.

---
+++ src/wwwroot/js/menu.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", (event) => {
+    document.querySelector(".mobile-sidebar-trigger").addEventListener("click", () => {
+        document.querySelector("#barra-lateral").classList.toggle("ativo");
+    });
+
+    document.querySelectorAll("#barra-lateral__informacoes > ul > li > a").forEach(e => {
+        e.addEventListener("click", () => {
+            if (document.querySelector("#barra-lateral").classList.contains("ativo")) {
+                document.querySelector("#barra-lateral").classList.remove("ativo");
+            }
+        });
+    });
+});
