0c5cdbe5d43fb5b22f03be6a321b2ae7d2041b0c
nixs-dev <joabewick@gmail.com>
2024-06-05 20:15:55 -0300

    .

---
+++ src/wwwroot/js/profile.js
@@ -0,0 +1,22 @@
+﻿const abas = {
+    "edicao": "menu__conteudo-edicao",
+    "notificacoes": "menu__conteudo-notificacoes"
+};
+
+function mudarAba(aba) {
+    document.querySelectorAll("#menu__conteudo > div.ativo").forEach((e) => {
+        e.classList.remove("ativo");
+    });
+
+    document.getElementById(abas[aba]).classList.add("ativo");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("menu__notificacao-opcao").addEventListener("click", () => {
+        mudarAba("notificacoes");
+    });
+
+    document.getElementById("menu__edicao-opcao").addEventListener("click", () => {
+        mudarAba("edicao");
+    });
+})
